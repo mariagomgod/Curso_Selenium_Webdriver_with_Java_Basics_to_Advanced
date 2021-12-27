93d75ee90259f21f91017b6b324dda55da85c88d
Maria Gomez <maria_gg86@yahoo.es>
2021-12-27 16:29:42 +0100

    Understanding the core concept of Browser driver classesand Webdriver Interface

---
+++ src/main/java/SelIntroduction.java
@@ -0,0 +1,17 @@
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SelIntroduction {
+
+    public static void main(String[] args) {
+
+        //Invoking Browser
+        //Chrome - ChromeDriver -> Methods close get
+        //Firefox - FirefoxDriver -> Methods close get
+        //Safari SaariDrier -> Methods close get
+        //WebDriver close get
+        //Webdriver methods + class methods
+        WebDriver driver = new ChromeDriver();
+    }
+}
