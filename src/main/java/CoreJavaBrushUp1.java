141be927865c79f70d4509873f28f007c1c24c9d
Maria Gomez <maria_gg86@yahoo.es>
2021-12-28 19:10:45 +0100

    Introduction to For loop

--- src/main/java/CoreJavaBrushUp1.java
+++ src/main/java/CoreJavaBrushUp1.java
@@ -17,8 +17,19 @@ public class CoreJavaBrushUp1 {
         arr[3] = 5;
         arr[4] = 6;
 
-        int[] arr2 = {1, 2 , 4, 5, 6};
+        int[] arr2 = {1, 2 , 4, 5, 6, 8, 100, 122};
 
         System.out.println(arr2[2]);
+
+        // For loop
+        for(int i = 0; i < arr2.length; i++) {
+            System.out.println(arr2[i]);
+        }
+
+        String[] name = {"Raul", "Shetty", "Selenium"};
+
+        for(int i = 0; i < name.length; i++) {
+            System.out.println(name[i]);
+        }
     }
 }
