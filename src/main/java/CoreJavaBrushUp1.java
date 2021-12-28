ae80b62a563c523cc7465bc5e9195ddd183ef5fc
Maria Gomez <maria_gg86@yahoo.es>
2021-12-28 18:56:50 +0100

    How to initialize and retireve the values of an Array

--- src/main/java/CoreJavaBrushUp1.java
+++ src/main/java/CoreJavaBrushUp1.java
@@ -8,5 +8,17 @@ public class CoreJavaBrushUp1 {
         boolean myCard = true;
 
         System.out.println(myNum + " is the value stored in the myNum variable");
+
+        // Arrays
+        int[] arr = new int[5];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 4;
+        arr[3] = 5;
+        arr[4] = 6;
+
+        int[] arr2 = {1, 2 , 4, 5, 6};
+
+        System.out.println(arr2[2]);
     }
 }
