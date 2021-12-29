f16df6bbc6e7ed1caefab0f425fbf52c319fcbe1
Maria Gomez <maria_gg86@yahoo.es>
2021-12-29 18:20:32 +0100

    How to create methods and access methods using Java class objects

---
+++ src/main/java/MethodsDemo.java
@@ -0,0 +1,15 @@
+public class MethodsDemo {
+
+    public static void main(String[] args) {
+        MethodsDemo d = new MethodsDemo();
+        String name = d.getData();
+        System.out.println(name);
+        MethodsDemo2 d1 = new MethodsDemo2();
+        d1.getUserData();
+    }
+
+    public static String getData() {
+        System.out.println("hello world");
+        return "Rahul Shetty";
+    }
+}
