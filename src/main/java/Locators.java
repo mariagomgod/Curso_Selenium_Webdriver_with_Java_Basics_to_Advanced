b1ffa07b549e16ada437499864786a5849c719eb
Maria Gomez <maria_gg86@yahoo.es>
2022-01-01 15:31:36 +0100

    Identifying the Web elements with id and name locators

---
+++ src/main/java/Locators.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locators {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        driver.findElement(By.id("inputUsername")).sendKeys("rahul");
+        driver.findElement(By.id("inputPassword")).sendKeys("hello123");
+        driver.findElement(By.className("signInBtn")).click();
+    }
+}
