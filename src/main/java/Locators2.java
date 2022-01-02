84d84dc8ec41b83ebc4b4fc850c91fb0f421919e
Maria Gomez <maria_gg86@yahoo.es>
2022-01-02 15:17:16 +0100

    Parse the String with Java methods to get the password dynamically from the page

--- src/main/java/Locators2.java
+++ src/main/java/Locators2.java
@@ -10,18 +10,37 @@ public class Locators2 {
     public static void main(String[] args) throws InterruptedException {
 
         String name = "rahul";
-
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        String password = getPassword(driver);
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
         driver.findElement(By.id("inputUsername")).sendKeys(name);
-        driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.name("inputPassword")).sendKeys(password);
         driver.findElement(By.className("signInBtn")).click();
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         System.out.println(driver.findElement(By.tagName("p")).getText());
         Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "You are successfully logged in.");
         Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText(), "Hello " + name + ",");
         driver.findElement(By.xpath("//*[text()='Log Out']")).click();
         driver.close();
+    }
+
+    public static String getPassword(WebDriver driver) throws InterruptedException {
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        driver.findElement(By.linkText("Forgot your password?")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+        String passwordText = driver.findElement(By.cssSelector("form p")).getText();
+        //Please use temporary password 'raulshettyacademy' to Login.
+        //0th index - Please use temporary password
+        //1st index - raulshettyacademy' to Login.
+
+        //0th index - raulshettyacademy
+        //1st index - to Login.
+        String[] passwordArray = passwordText.split("'");
+        //String[] passwordArray2 = passwordArray[1].split("'");
+        //passwordArray2[0]
+        String password = passwordArray[1].split("'")[0];
+        return password;
     }
 }
