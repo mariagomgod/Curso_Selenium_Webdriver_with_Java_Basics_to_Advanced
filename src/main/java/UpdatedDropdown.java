3fe99f609047d90809b6a9979937739564c0a1a2
Maria Gomez <maria_gg86@yahoo.es>
2022-01-09 20:17:36 +0100

    Updated lecture on latest Dropdown looping UI

---
+++ src/main/java/UpdatedDropdown.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class UpdatedDropdown {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        driver.findElement(By.id("divpaxinfo")).click();
+        Thread.sleep(2000L);
+
+        int timesClicked;
+        for (timesClicked = 1; timesClicked < 5; timesClicked++) {
+            driver.findElement(By.id("hrefIncAdt")).click(); // 4 times clicked (4 adults)
+        }
+
+        driver.findElement(By.id("btnclosepaxoption")).click();
+        Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(), "5 Adult");
+        System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+    }
+}
