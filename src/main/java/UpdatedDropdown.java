28a99a691402b15b48490e92cc74b50b15d29efe
Maria Gomez <maria_gg86@yahoo.es>
2022-03-13 17:14:02 +0100

    Asserts

--- src/main/java/UpdatedDropdown.java
+++ src/main/java/UpdatedDropdown.java
@@ -9,9 +9,11 @@ public class UpdatedDropdown {
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
-        System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+        Assert.assertFalse(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+        //System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
         driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click();
         System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+        Assert.assertTrue(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
 
         // Count the number of checkboxes
         System.out.println(driver.findElements(By.cssSelector("input[type='checkbox']")).size());
