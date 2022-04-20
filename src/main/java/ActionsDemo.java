e9d53fd48b8f1f67401b5977a964c1a578609257
Maria Gomez <maria_gg86@yahoo.es>
2022-04-20 18:12:07 +0200

    Window handle concepts

--- src/main/java/ActionsDemo.java
+++ src/main/java/ActionsDemo.java
@@ -1,5 +1,7 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
@@ -8,9 +10,20 @@ public class ActionsDemo {
     public static void main(String[] args) {
 
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.get("https://www.amazon.com/");
 
         Actions a = new Actions(driver);
-        a.moveToElement(driver.findElement(By.cssSelector("a[id='nav-link-accountList']"))).build().perform();
+        WebElement move = driver.findElement(By.cssSelector("a[id='nav-link-accountList']"));
+
+        a.moveToElement(driver.findElement(By.id("twotabsearchtextbox")))
+                .click()
+                .keyDown(Keys.SHIFT)
+                .sendKeys("hello")
+                .doubleClick()
+                .build()
+                .perform();
+        // Move to a specific element
+        a.moveToElement(move).contextClick().build().perform();
     }
 }
