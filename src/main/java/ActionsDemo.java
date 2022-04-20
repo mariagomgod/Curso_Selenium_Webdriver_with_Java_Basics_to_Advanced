eb086dc80654f58570a7e756eca31afa532d1967
Maria Gomez <maria_gg86@yahoo.es>
2022-04-20 16:47:26 +0200

    Hnadling mouse interactions

---
+++ src/main/java/ActionsDemo.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class ActionsDemo {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.com/");
+
+        Actions a = new Actions(driver);
+        a.moveToElement(driver.findElement(By.cssSelector("a[id='nav-link-accountList']"))).build().perform();
+    }
+}
