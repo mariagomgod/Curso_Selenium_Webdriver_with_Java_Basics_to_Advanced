d95e549389f5dafcbd54785ea363bf8c38cbf77c
Maria Gomez <maria_gg86@yahoo.es>
2022-04-24 19:30:57 +0200

    Handling calendar UI travel websites. Example solved

--- src/main/java/Calendar.java
+++ src/main/java/Calendar.java
@@ -11,16 +11,22 @@ public class Calendar {
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.path2usa.com/travel-companions");
-        // August 23
+        // April 24
         driver.findElement(By.xpath(".//*[@id='travel_date']")).click();
+
+        while (!driver.findElement(By.cssSelector("[class='datepicker-days'][class='datepicker-switch']")).getText().contains("April")) {
+            driver.findElement(By.cssSelector("[class='datepicker-days'] th[class='next']")).click();
+        }
+
         List<WebElement> dates = driver.findElements(By.className("day"));
 
+        // Grab common attribute: put into list iterate.
         int count = driver.findElements(By.className("day")).size();
 
         for (int i = 0; i < count; i++) {
             String text = driver.findElements(By.className("day")).get(i).getText();
 
-            if (text.equalsIgnoreCase("23")) {
+            if (text.equalsIgnoreCase("24")) {
                 driver.findElements(By.className("day")).get(i).click();
                 break;
             }
