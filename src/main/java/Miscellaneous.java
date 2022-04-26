ea205042201e30722c226a76cb3bb08b04faa70d
Maria Gomez <maria_gg86@yahoo.es>
2022-04-26 17:35:31 +0200

    How to take Screenshots in Selenium

--- src/main/java/Miscellaneous.java
+++ src/main/java/Miscellaneous.java
@@ -1,9 +1,16 @@
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Miscellaneous {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -14,6 +21,9 @@ public class Miscellaneous {
         // login page - verify login url
 
         driver.get("https://www.google.com/");
+
+        File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(src, new File("C:\\Users\\María\\screenshot.png"));
 
     }
 }
