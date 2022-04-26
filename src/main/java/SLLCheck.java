d838627732026b9a660ca7dc1f67ea645c0827fd
Maria Gomez <maria_gg86@yahoo.es>
2022-04-26 16:09:04 +0200

    Map<String, Object> prefs = new HashMap<String, Object>();

--- src/main/java/SLLCheck.java
+++ src/main/java/SLLCheck.java
@@ -1,13 +1,25 @@
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SLLCheck {
 
     public static void main(String[] args) {
 
         ChromeOptions options = new ChromeOptions();
-        options.setAcceptInsecureCerts(true);
+        //options.setAcceptInsecureCerts(true);
+
+        // Open Chrome extensions (plugins...)
+        Proxy proxy = new Proxy();
+        proxy.setHttpProxy("ipaddress:4444");
+        options.setCapability("proxy", proxy);
+        Map<String, Object> prefs = new HashMap<String, Object>();
+        prefs.put("download.default_directory", "/directory/path");
+        options.setExperimentalOption("prefs", prefs);
 
         WebDriver driver = new ChromeDriver(options);
 
