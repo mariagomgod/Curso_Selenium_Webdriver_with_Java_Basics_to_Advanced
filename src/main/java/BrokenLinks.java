4e7223f413ea9b6e4ba3c926a7ebd64528493ecc
Maria Gomez <maria_gg86@yahoo.es>
2022-04-28 18:22:25 +0200

    Open Connection method to identify status codes of the links

--- src/main/java/BrokenLinks.java
+++ src/main/java/BrokenLinks.java
@@ -2,9 +2,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class BrokenLinks {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException, IOException {
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
@@ -13,6 +18,11 @@ public class BrokenLinks {
         // 1.- To get all URL's tied up to the links
         // Java methods will call URL´s and get you the status code
         // If status code >400 then that url is not working -> link tied up to url is broken
-        String url = driver.findElement(By.cssSelector("a[href*='soapui']")).getAttribute("href");
+        String url = driver.findElement(By.cssSelector("a[href*='brokenlink']")).getAttribute("href");
+        HttpURLConnection connection = (HttpURLConnection)new URL(url).openConnection();
+        connection.setRequestMethod("HEAD");
+        connection.connect();
+        int responseCode = connection.getResponseCode();
+        System.out.println(responseCode);
     }
 }
