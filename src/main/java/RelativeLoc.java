c6d12295b5448ab3c675824d428d88d3f8556fe0
Maria Gomez <maria_gg86@yahoo.es>
2022-05-07 17:37:10 +0200

    Relative locator above()

---
+++ src/main/java/RelativeLoc.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.openqa.selenium.support.locators.RelativeLocator.*;
+
+
+public class RelativeLoc {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/angularpractice/");
+        WebElement nameEditBox = driver.findElement(By.cssSelector("[name='name']"));
+        System.out.println(driver.findElement(with(By.tagName("label")).above(nameEditBox)).getText());
+
+    }
+}
