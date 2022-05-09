5d6415316aa4ee3d45b408b334f84a039758615d
Maria Gomez <maria_gg86@yahoo.es>
2022-05-09 14:06:18 +0200

    Invoking Multiple Windows/Tabs from Selenium

---
+++ src/main/java/NewWindow.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class NewWindow {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/angularpractice/");
+        driver.switchTo().newWindow(WindowType.TAB);
+        Set<String> handles = driver.getWindowHandles();
+        Iterator<String> it = handles.iterator();
+        String parentWindowId = it.next();
+        String childWindow = it.next();
+        driver.switchTo().window(childWindow);
+        driver.get("https://rahulshettyacademy.com/");
+        String courseName = driver.findElements(By.cssSelector("a[href*='https://courses.rahulshettyacademy.com/p']"))
+                .get(1).getText();
+        driver.switchTo().window(parentWindowId);
+        driver.findElement(By.cssSelector("[name='name']")).sendKeys(courseName);
+        //driver.quit();
+    }
+}
